50690e2bd838036942f7f9e170a1e39e446bcdbc
geenyzk <azukagenius09@gmail.com>
2025-09-08 10:15:38 +0100

    Implement user registration and update login page

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import CreateUserForm, LoginForm
 
 # Create your views here.
 def home(request):
@@ -6,3 +7,17 @@ def home(request):
 
 def login(request):
     return render(request, 'pages/login.html')
+
+def register(request):
+    # Make a form from forms.py
+    
+    form = CreateUserForm()
+    if request.method == 'POST':
+        # Make a form with the data filled by the user
+
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return render(request, 'pages/register.html', context)
+
